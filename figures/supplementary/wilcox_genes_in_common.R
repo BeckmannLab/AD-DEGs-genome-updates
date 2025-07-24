c6c49db145c5a0524fbd6f32a87780412d0a68f3
anllund <54270095+anllund@users.noreply.github.com>
2025-07-24 10:45:20 -0400

    Update wilcox_genes_in_common.R

--- figures/supplementary/wilcox_genes_in_common.R
+++ figures/supplementary/wilcox_genes_in_common.R
@@ -1,25 +1,35 @@
 library(ggplot2)
 
-matrixhg18 = readRDS("hg18_for_wilcox.RDS") #see AD-DEGs-genome-updates/misc/ref_for_wilcox.R
-matrixhg19 = readRDS("hg19_for_wilcox.RDS") #see AD-DEGs-genome-updates/misc/ref_for_wilcox.R
-matrixv30 = readRDS("v30_for_wilcox.RDS") #see AD-DEGs-genome-updates/misc/ref_for_wilcox.R
-matrixv43 = readRDS("v43_for_wilcox.RDS") #see AD-DEGs-genome-updates/misc/ref_for_wilcox.R
-matrixT2T = readRDS("T2T_for_wilcox.RDS") #see AD-DEGs-genome-updates/misc/ref_for_wilcox.R
+matrixhg18  <- readRDS("hg18_for_wilcox.RDS")
+matrixhg19  <- readRDS("hg19_for_wilcox.RDS")
+matrixv30   <- readRDS("v30_for_wilcox.RDS")
+matrixv43   <- readRDS("v43_for_wilcox.RDS")
+matrixT2T   <- readRDS("T2T_for_wilcox.RDS")
 
-
-mats <- list(hg18 = matrixhg18, hg19 = matrixhg19, v30 = matrixv30, v43 = matrixv43, T2T = matrixT2T)
+mats <- list(
+  hg18 = matrixhg18,
+  hg19 = matrixhg19,
+  v30  = matrixv30,
+  v43  = matrixv43,
+  T2T  = matrixT2T
+)
 
 ## Functions ------------------------------------------------------------------
+
+# Align genes and samples common to both matrices and return matched subsets
 align_pair <- function(m1, m2) {
-  g <- rownames(m1)[rownames(m1) %in% rownames(m2)]          # genes
-  s <- colnames(m1)[colnames(m1) %in% colnames(m2)]          # samples
-  list(a = m1[g, s, drop = FALSE],
-       b = m2[g, s, drop = FALSE])
+  g <- rownames(m1)[rownames(m1) %in% rownames(m2)]
+  s <- colnames(m1)[colnames(m1) %in% colnames(m2)]
+  list(
+    a = m1[g, s, drop = FALSE],
+    b = m2[g, s, drop = FALSE]
+  )
 }
 
+# Perform paired Wilcoxon tests on each gene and compile results with adjustments
 run_pair <- function(m1, m2, lbl) {
   p   <- align_pair(m1, m2)
-  tst <- perform_tests(p$a, p$b)                 # list of htest objects
+  tst <- perform_tests(p$a, p$b)
   pv  <- sapply(tst, `[[`, "p.value")
   vv  <- sapply(tst, `[[`, "statistic")
 
@@ -35,10 +45,11 @@ run_pair <- function(m1, m2, lbl) {
   out
 }
 
+# Execute paired Wilcoxon test for each gene, returning a list of htest objects
 perform_tests <- function(mat1, mat2) {
-  stopifnot(identical(dim(mat1), dim(mat2)))  # safety check
+  stopifnot(identical(dim(mat1), dim(mat2)))
   gene_names <- rownames(mat1)
-  
+
   res <- vector("list", length(gene_names))
   for (i in seq_along(gene_names)) {
     res[[i]] <- wilcox.test(
@@ -51,11 +62,12 @@ perform_tests <- function(mat1, mat2) {
 }
 
 ## Comparison plan ------------------------------------------------------------
+
 pairs <- data.frame(
-  a1    = c("hg18","hg18","hg18","hg18",
-            "hg19","hg19","hg19",
-            "v30","v30",
-            "v43"),
+  a1 = c("hg18","hg18","hg18","hg18",
+         "hg19","hg19","hg19",
+         "v30","v30",
+         "v43"),
   a2    = c("hg19","v30","v43","T2T",
             "v30","v43","T2T",
             "v43","T2T",
@@ -76,21 +88,22 @@ pairs <- data.frame(
 )
 
 ## Run tests ------------------------------------------------------------------
-results_list <- vector("list", nrow(pairs))
 
+results_list <- vector("list", nrow(pairs))
 for (i in seq_len(nrow(pairs))) {
-	print(i)
+  print(i)
   results_list[[i]] <- run_pair(
-    mats[[ pairs$a1[i] ]],
-    mats[[ pairs$a2[i] ]],
+    mats[[pairs$a1[i]]],
+    mats[[pairs$a2[i]]],
     pairs$label[i]
   )
 }
 
 results <- do.call(rbind, results_list)
-results <- results[complete.cases(results), ]      # drop rows with any NA
+results <- results[complete.cases(results), ]
 
 ## Plot -----------------------------------------------------------------------
+
 order_vec <- c(
   "GRCh37/NCBI36",
   "GRCh38.p12/NCBI36",
@@ -103,15 +116,18 @@ order_vec <- c(
   "CHM13v2.0/GRCh38.p12",
   "CHM13v2.0/GRCh38.p13"
 )
-
 results$assembly <- factor(results$assembly, levels = order_vec)
+
 g <- ggplot(results, aes(assembly, fill = Significant)) +
   geom_bar(position = "dodge") +
   labs(x = "Reference Comparisons", y = "Expressed Gene Count") +
-  scale_fill_manual(values = c("red", "blue"),
-                    labels = c("> 0.05", "<= 0.05"),
-                    name   = "Bonferroni") +
+  scale_fill_manual(
+    values = c("red", "blue"),
+    labels = c("> 0.05", "<= 0.05"),
+    name   = "Bonferroni"
+  ) +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 50, hjust = 0.8))
 
 ggsave(plot_file, g, useDingbats = FALSE)
+
