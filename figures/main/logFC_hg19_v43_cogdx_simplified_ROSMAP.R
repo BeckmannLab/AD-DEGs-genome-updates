5a8f1836cdd004ff4163760f6b2c4c97f1eb33f7
anllund <54270095+anllund@users.noreply.github.com>
2025-07-24 16:51:05 -0400

    Create logFC_hg19_v43_cogdx_simplified_ROSMAP.R

---
+++ figures/main/logFC_hg19_v43_cogdx_simplified_ROSMAP.R
@@ -0,0 +1,201 @@
+
+
+## Clear the global environment
+rm(list = ls())
+
+library(ggplot2)
+library(data.table)
+############################
+####DE AD results first
+################################
+# Clear all objects from the workspace
+rm(list=ls())
+
+# Define file paths for differential expression results for each genome assembly
+path_hg18= #path to DE results
+path_hg19= #path to DE results
+path_v30= #path to DE results
+path_v43= #path to DE results
+path_T2T= #path to DE results
+
+# Specify phenotypes or tests to process
+phenos=c("braaksc_simplified","ceradsc_defvsctl","cogdx_simplified","cts_mmse30")
+
+# List of genome assemblies
+assemblies=c("hg18","hg19","v30","v43","T2T")
+
+# Generate all unique pairs of assemblies
+assembliesComb = t(combn(assemblies,2))
+assembliesComb2=assembliesComb[assembliesComb[,2]=="T2T",]
+assembliesComb=assembliesComb[assembliesComb[,2]!="T2T",]
+assembliesComb=rbind(assembliesComb,assembliesComb2[,2:1])
+assembliesComb=cbind(assembliesComb,apply(assembliesComb,1,function(x){paste0(x[1],x[2])}))
+
+# Create a grid of assembly pairs and phenotypes
+grid=expand.grid(assembliesComb[,3],phenos)
+grid$Var3=paste0(grid$Var1,"DE_",grid$Var2)
+grid$Var4=as.character(grid$Var2)
+grid$Var4[grid$Var4=="ceradsc_defvsctl"]="ceradsc_test.txt"
+colnames(grid)=c("assemblies","pheno","assemblies_DE_pheno","pheno_fixed")
+colnames(assembliesComb)=c("assembly1","assembly2","assemblies")
+grid=merge(grid,assembliesComb,by="assemblies")
+
+# Prepare paths for each assembly
+grid3=as.data.frame(assemblies)
+grid3$assemblies_path=paste("path",grid3$assemblies, sep = "_")
+
+# Loop through each assembly to read and merge DE files
+for(i in 1:nrow(grid3)){
+path=get(grid3[i,"assemblies_path"])
+DE_files=list.files(path,pattern="^DE")
+all=data.table()
+    for(DE_file in DE_files){
+        tmp=fread(paste0(path,DE_file))
+        phenotype=unlist(lapply(strsplit(DE_file,"_"),function(x){paste0(x[20],"_",x[21])}))
+        colnames(tmp)[2:ncol(tmp)]=paste0(colnames(tmp)[2:ncol(tmp)],"|",phenotype)
+        if(nrow(all)<1){
+            all=tmp
+        }else{
+            all=merge(all,tmp,by="V1",all=TRUE)
+        }}
+colnames(all)=paste0(colnames(all),"_",grid3[i,"assemblies"])
+colnames(all)[1]="gene_id"
+value_assigned=as.character(grid3[i, "assemblies"])
+assign(value_assigned,all)
+}
+
+# Merge results from all assemblies into one data frame
+all_results_AD=merge(hg18,hg19,by="gene_id",all=TRUE)
+all_results_AD=merge(all_results_AD,v30,by="gene_id",all=TRUE)
+all_results_AD=merge(all_results_AD,v43,by="gene_id",all=TRUE)
+all_results_AD=merge(all_results_AD,T2T,by="gene_id",all=TRUE)
+all_results_AD=as.data.frame(all_results_AD)
+
+# Reinitialize combinations for significance categorization
+assembliesComb = t(combn(assemblies,2))
+assembliesComb=cbind(assembliesComb,apply(assembliesComb,1,function(x){paste0(x[2],x[1])}))
+
+# Recreate grid for significance loop
+grid=expand.grid(assembliesComb[,3],phenos)
+grid$Var3=paste0(grid$Var1,"DE_",grid$Var2)
+grid$Var4=as.character(grid$Var2)
+grid$Var4[grid$Var4=="ceradsc_defvsctl"]="ceradsc_test.txt"
+colnames(grid)=c("assemblies","pheno","assemblies_DE_pheno","pheno_fixed")
+colnames(assembliesComb)=c("assembly1","assembly2","assemblies")
+grid=merge(grid,assembliesComb,by="assemblies")
+
+# Copy merged results for analysis
+all_results = all_results_AD
+
+# Loop to categorize significance across assembly pairs for each phenotype
+for(i in 1:nrow(grid)){
+    # Define output column for this comparison
+    outCol=paste0(grid[i,"assembly2"],grid[i,"assembly1"],"DE_",grid[i,"pheno"])
+    # Identify adjusted p-value columns for each assembly
+    colOfInterest1=paste0("adj.P.Val|",grid[i,"pheno_fixed"],"_",grid[i,"assembly1"])
+    colOfInterest2=paste0("adj.P.Val|",grid[i,"pheno_fixed"],"_",grid[i,"assembly2"])
+    # Initialize output column
+    all_results[,outCol]=""
+
+    # Default label when both present but not significant
+    all_results[!is.na(all_results[,colOfInterest1]) & !is.na(all_results[,colOfInterest2]),outCol]="notSignif"
+
+    # Cases where one is NA and the other significant
+    all_results[which(is.na(all_results[, colOfInterest1]) & all_results[, colOfInterest2] <= 0.05), outCol] = paste0(grid[i,"assembly2"],"Signif")
+    all_results[which(is.na(all_results[,colOfInterest2]) & all_results[,colOfInterest1]<=0.05),outCol]=paste0(grid[i,"assembly1"],"Signif")
+
+    # Both significant
+    all_results[which(all_results[,colOfInterest1]<=0.05 & all_results[,colOfInterest2]<=0.05),outCol]="bothSignif"
+
+    # One significant, one not
+    all_results[which(all_results[,colOfInterest1]<=0.05 & all_results[,colOfInterest2]>0.05),outCol]=paste0(grid[i,"assembly1"],"Signif")
+    all_results[which(all_results[,colOfInterest1]>0.05 & all_results[,colOfInterest2]<=0.05),outCol]=paste0(grid[i,"assembly2"],"Signif")
+
+    # Cases where one is NA but not significant
+    all_results[which(is.na(all_results[,colOfInterest1]) & all_results[,colOfInterest2]>0.05),outCol]="notSignif"
+    all_results[which(is.na(all_results[,colOfInterest2]) & all_results[,colOfInterest1]>0.05),outCol]="notSignif"
+
+    # Replace empty strings with NA
+    all_results[all_results[,outCol]=="",outCol]=NA
+
+    # Set factor levels for categorization
+    all_results[,outCol]=factor(all_results[,outCol],levels=c(paste0(grid[i,"assembly1"],"Signif"),paste0(grid[i,"assembly2"],"Signif"),"bothSignif","notSignif"))
+}
+
+# Convert to data frame and save results
+all_results=as.data.frame(all_results)
+
+all_results$v43hg19DE_cogdx_simplified = gsub("v43Signif", "GRCh38.p13 significant",all_results$v43hg19DE_cogdx_simplified )
+
+all_results$v43hg19DE_cogdx_simplified = gsub("hg19Signif", "GRCh37 significant",all_results$v43hg19DE_cogdx_simplified )
+
+all_results$v43hg19DE_cogdx_simplified = gsub("bothSignif", "Both references significant",all_results$v43hg19DE_cogdx_simplified )
+
+all_results$v43hg19DE_cogdx_simplified = gsub("notSignif", "Neither reference significant",all_results$v43hg19DE_cogdx_simplified )
+
+
+
+g <- ggplot(all_results, aes(`logFC|cogdx_simplified_hg19`, `logFC|cogdx_simplified_v43`)) +
+  # layer 1: neither (grey) + both (green)
+  geom_point(
+    data = subset(all_results,
+      v43hg19DE_cogdx_simplified %in% c("Neither reference significant","Both references significant")
+    ),
+    aes(color = v43hg19DE_cogdx_simplified),
+    size  = 2.5,
+    alpha = 0.4
+  ) +
+  # layer 2: older (red) + newer (blue) ON TOP
+  geom_point(
+    data = subset(all_results,
+      v43hg19DE_cogdx_simplified %in% c("GRCh37 significant","GRCh38.p13 significant")
+    ),
+    aes(color = v43hg19DE_cogdx_simplified),
+    size  = 2.5,
+    alpha = 0.4
+  ) +
+  # scale_x_continuous(expand = expansion(add = 0.05)) +
+  # scale_y_continuous(expand = expansion(add = 0.05)) +
+  scale_color_manual(
+    name = "AD DE Status",
+    breaks = c(
+      "GRCh37 significant",
+      "GRCh38.p13 significant",
+      "Both references significant",
+      "Neither reference significant"
+    ),
+    values = c(
+      "Neither reference significant"  = "grey80",
+      "Both references significant"    = "darkgreen",
+      "GRCh37 significant"             = "red",
+      "GRCh38.p13 significant"         = "blue"
+    ),
+    guide = guide_legend(
+      override.aes   = list(size = 6),   # slightly smaller symbols
+      title.position = "top",
+      title.hjust    = 0
+    )
+  ) +
+  labs(
+    x = "LogFC GRCh37",
+    y = "LogFC GRCh38.p13"
+  ) +
+  theme_bw(base_size = 16) +
+  theme(
+    axis.title        = element_text(size = 22),
+    axis.text         = element_text(size = 20),
+
+    # legend styling: a bit smaller than “supersized”
+    legend.position      = c(0.36, 0.83),
+    legend.direction     = "vertical",
+    legend.title         = element_text(size = 20),     # slightly reduced
+    legend.text          = element_text(size = 16),     # slightly reduced
+    legend.key.size      = unit(1.5, "lines"),          # back down a notch
+    legend.background    = element_rect(color = "black", fill = "white")
+    # legend.title.align   = 0,
+    # panel.spacing      = unit(0.5, "lines"),
+    # plot.margin        = margin(10, 10, 10, 10)
+  ) + coord_fixed(    xlim   = c(-0.6,  0.6),
+    ylim   = c(-0.6,  0.8))
+
+ggsave("logFC_hg19_v43_cogdx_simplified.pdf",g, width = 7, height = 7, useDingbats=FALSE)
