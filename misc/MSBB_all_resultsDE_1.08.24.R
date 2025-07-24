ac1773b3cca663d9f55e5c45f1b8ecb264f773a2
anllund <54270095+anllund@users.noreply.github.com>
2025-07-24 14:38:23 -0400

    Create MSBB_all_resultsDE_1.08.24.R

---
+++ misc/MSBB_all_resultsDE_1.08.24.R
@@ -0,0 +1,71 @@
+## Clear the global environment
+rm(list = ls())
+
+library(data.table)
+############################
+####DE AD results first
+################################
+# Clear all objects from the workspace
+rm(list=ls())
+
+# Define file paths for differential expression results for each genome assembly
+path_hg18= #path to DE results
+path_hg19= #path to DE results
+path_v30= #path to DE results
+path_v43= #path to DE results
+path_T2T= #path to DE results
+
+# Specify phenotypes or tests to process
+phenos=c("CDR_simplified", "CERJ_defvsctl","PlaqueMean_test.txt")
+
+# List of genome assemblies
+assemblies=c("hg18","hg19","v30","v43","T2T")
+
+# Generate all unique pairs of assemblies
+assembliesComb = t(combn(assemblies,2))
+assembliesComb2=assembliesComb[assembliesComb[,2]=="T2T",]
+assembliesComb=assembliesComb[assembliesComb[,2]!="T2T",]
+assembliesComb=rbind(assembliesComb,assembliesComb2[,2:1])
+assembliesComb=cbind(assembliesComb,apply(assembliesComb,1,function(x){paste0(x[1],x[2])}))
+
+# Create a grid of assembly pairs and phenotypes
+grid=expand.grid(assembliesComb[,3],phenos)
+grid$Var3=paste0(grid$Var1,"DE_",grid$Var2)
+grid$Var4=as.character(grid$Var2)
+colnames(grid)=c("assemblies","pheno","assemblies_DE_pheno","pheno_fixed")
+colnames(assembliesComb)=c("assembly1","assembly2","assemblies")
+grid=merge(grid,assembliesComb,by="assemblies")
+
+# Prepare paths for each assembly
+grid3=as.data.frame(assemblies)
+grid3$assemblies_path=paste("path",grid3$assemblies, sep = "_")
+
+# Loop through each assembly to read and merge DE files
+for(i in 1:nrow(grid3)){
+  path=get(grid3[i,"assemblies_path"])
+  DE_files=list.files(path,pattern="^DE")
+  all=data.table()
+  # Read and merge each DE file
+  for(DE_file in DE_files){
+      tmp=fread(paste0(path,DE_file))
+      phenotype=unlist(lapply(strsplit(DE_file,"_"),function(x){paste0(x[11],"_",x[12])}))
+      colnames(tmp)[2:ncol(tmp)]=paste0(colnames(tmp)[2:ncol(tmp)],"|",phenotype)
+      if(nrow(all)<1){
+          all=tmp
+      }else{
+          all=merge(all,tmp,by="V1",all=TRUE)
+      }}
+  colnames(all)=paste0(colnames(all),"_",grid3[i,"assemblies"])
+  colnames(all)[1]="gene_id"
+  value_assigned=as.character(grid3[i, "assemblies"])
+  assign(value_assigned,all)
+}
+
+# Merge results from all assemblies into one data frame
+all_results_AD=merge(hg18,hg19,by="gene_id",all=TRUE)
+all_results_AD=merge(all_results_AD,v30,by="gene_id",all=TRUE)
+all_results_AD=merge(all_results_AD,v43,by="gene_id",all=TRUE)
+all_results_AD=merge(all_results_AD,T2T,by="gene_id",all=TRUE)
+all_results_AD=as.data.frame(all_results_AD)
+
+saveRDS(all_results_AD, "MSBB_all_resultsDE_1.08.24.RDS")
