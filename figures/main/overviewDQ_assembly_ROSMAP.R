37b5f09e88a845f7a447dcae6514d2b879a9d766
anllund <54270095+anllund@users.noreply.github.com>
2025-07-25 17:35:17 -0400

    Update overviewDQ_assembly_ROSMAP.R

--- figures/main/overviewDQ_assembly_ROSMAP.R
+++ figures/main/overviewDQ_assembly_ROSMAP.R
@@ -1,12 +1,17 @@
 #Figure 2C
+
+# ==== Full Path to Save Directory (Edit or Remove Before Publishing) ====
+output_dir <- "/assembly"
+
 # ==== Input & Output Filenames (relative to your working directory) ====
-input_rds_all    <- "with_signif_categories_results_assembly_ROSMAP_1.30.24.RDS" #see AD-DEGs-genome-updates/misc/with_signif_categories_results_assembly_ROSMAP_1.30.24.RDS
-input_rds_msbb   <- "all_info_MSBB.RDS"
-input_rds_rosmap <- "all_info_ROSMAP.RDS"
+input_rds_all    <- "with_signif_categories_results_assembly_ROSMAP_1.30.24.RDS" #see AD-DEGs-genome-updates/misc/with_signif_categories_results_assembly_ROSMAP_1.30.24.R
+input_rds_msbb   <- "with_coding_status_all_info_MSBB.RDS" #see AD-DEGs-genome-updates/misc/with_coding_status_all_info_MSBB.R
+input_rds_rosmap <- "all_info_ROSMAP.RDS" #see AD-DEGs-genome-updates/misc/all_info_ROSMAP.R
 output_pdf       <- file.path(output_dir, "overviewDQ_assembly_ROSMAP.pdf")
 
 # ==== Libraries ====
 library(ggplot2)
+library(dlpyr)
 
 # ==== Functions for Data Preparation & Plotting ====
 
@@ -24,9 +29,9 @@ set_assembly_levels <- function(df) {
   df$assembly <- factor(
     df$assembly,
     levels = c(
-      "GRCh37-NCBI36", "GRCh38.12-NCBI36", "GRCh38.13-NCBI36", "CHM13v2.0-NCBI36",
-      "GRCh38.12-GRCh37", "GRCh38.13-GRCh37", "CHM13v2.0-GRCh37",
-      "GRCh38.13-GRCh38.12", "CHM13v2.0-GRCh38.12", "CHM13v2.0-GRCh38.13"
+      "GRCh37-NCBI36", "GRCh38.p12-NCBI36", "GRCh38.p13-NCBI36", "CHM13v2.0-NCBI36",
+      "GRCh38.p12-GRCh37", "GRCh38.p13-GRCh37", "CHM13v2.0-GRCh37",
+      "GRCh38.p13-GRCh38.p12", "CHM13v2.0-GRCh38.p12", "CHM13v2.0-GRCh38.p13"
     )
   )
   df
@@ -62,10 +67,10 @@ create_overview_plot <- function(df) {
       legend.position      = c(0, 1),
       legend.justification = c(0, 1),
       legend.text          = element_text(size = 15),
-      legend.title         = element_text(size = 17)
+      legend.title         = element_text(size = 17),
+      legend.background    = element_rect(color = "black", fill = "white", linewidth = 0.5)
     )
 }
-
 # Function to save a plot with dingbats disabled
 save_plot_no_dingbats <- function(plot_obj, filename, width = 10, height = 8) {
   ggsave(
@@ -77,10 +82,14 @@ save_plot_no_dingbats <- function(plot_obj, filename, width = 10, height = 8) {
   )
 }
 
-# ========
+# ==== Main Workflow ====
 
 # Load data
+all_info_MSBB    <- readRDS(input_rds_msbb)
 info_all_ROSMAP  <- readRDS(input_rds_rosmap)
+
+
+# (You can also load `all` if needed: readRDS(input_rds_all))
 
 # Prepare data for plotting
 info_all_ROSMAP <- info_all_ROSMAP %>%
