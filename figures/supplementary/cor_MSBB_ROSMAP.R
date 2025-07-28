09b04bc3265bb7583f1712bb3f178df20c9d1ce7
anllund <54270095+anllund@users.noreply.github.com>
2025-07-28 15:33:37 -0400

    Update cor_MSBB_ROSMAP.R

--- figures/supplementary/cor_MSBB_ROSMAP.R
+++ figures/supplementary/cor_MSBB_ROSMAP.R
@@ -10,16 +10,16 @@ library(ggplot2)
 # Convert assembly comparison codes into human-readable labels
 rename_labels <- function(labels) {
   mapping <- c(
-    hg19_hg18 = "GRCh37/NCBI36",
-    v30_hg18  = "GRCh38.p12/NCBI36",
-    v43_hg18  = "GRCh38.p13/NCBI36",
-    v30_hg19  = "GRCh38.p12/GRCh37",
-    v43_hg19  = "GRCh38.p13/GRCh37",
-    v43_v30   = "GRCh38.p13/GRCh38.p12",
-    T2T_hg18  = "T2T-CHM13v2.0/NCBI36",
-    T2T_hg19  = "T2T-CHM13v2.0/GRCh37",
-    T2T_v30   = "T2T-CHM13v2.0/GRCh38.p12",
-    T2T_v43   = "T2T-CHM13v2.0/GRCh38.p13"
+    hg19_hg18 = "GRCh37-NCBI36",
+    v30_hg18  = "GRCh38.p12-NCBI36",
+    v43_hg18  = "GRCh38.p13-NCBI36",
+    v30_hg19  = "GRCh38.p12-GRCh37",
+    v43_hg19  = "GRCh38.p13-GRCh37",
+    v43_v30   = "GRCh38.p13-GRCh38.p12",
+    T2T_hg18  = "T2T-CHM13v2.0-NCBI36",
+    T2T_hg19  = "T2T-CHM13v2.0-GRCh37",
+    T2T_v30   = "T2T-CHM13v2.0-GRCh38.p12",
+    T2T_v43   = "T2T-CHM13v2.0-GRCh38.p13"
   )
   sapply(labels, function(x) {
     if (x %in% names(mapping)) mapping[[x]] else x
@@ -50,16 +50,16 @@ diag_df$metric    <- "LogFC"
 
 # Set factor levels for consistent ordering in plots
 level_order <- c(
-  "GRCh37/NCBI36",
-  "GRCh38.p12/NCBI36",
-  "GRCh38.p13/NCBI36",
-  "T2T-CHM13v2.0/NCBI36",
-  "GRCh38.p12/GRCh37",
-  "GRCh38.p13/GRCh37",
-  "T2T-CHM13v2.0/GRCh37",
-  "GRCh38.p13/GRCh38.p12",
-  "T2T-CHM13v2.0/GRCh38.p12",
-  "T2T-CHM13v2.0/GRCh38.p13"
+  "GRCh37-NCBI36",
+  "GRCh38.p12-NCBI36",
+  "GRCh38.p13-NCBI36",
+  "T2T-CHM13v2.0-NCBI36",
+  "GRCh38.p12-GRCh37",
+  "GRCh38.p13-GRCh37",
+  "T2T-CHM13v2.0-GRCh37",
+  "GRCh38.p13-GRCh38.p12",
+  "T2T-CHM13v2.0-GRCh38.p12",
+  "T2T-CHM13v2.0-GRCh38.p13"
 )
 diag_df$reference <- factor(diag_df$reference, levels = level_order)
 
