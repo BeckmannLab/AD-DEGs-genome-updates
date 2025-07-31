996fa460c67e1db9ee110ab8939a96727ee930d5
anllund <54270095+anllund@users.noreply.github.com>
2025-07-31 17:53:19 -0400

    Update msigdb_interaction_ROSMAP.R

--- interaction/msigdb_interaction_ROSMAP.R
+++ interaction/msigdb_interaction_ROSMAP.R
@@ -4,9 +4,9 @@ library(dplyr)
 library(tidyr)
 library(stringr)
 
-#function
+
 format_for_gsea = function(df){
-	#######input for this is a two column dataframe where the first column are gene symbols and second column DE status which is either Signif or notSignif. if you want to do gene IDs you need to switch out. output is two column dataframe where first column is gene symbols second column is signif or not signif
+	#######input_rosmap for this is a two column dataframe where the first column are gene symbols and second column DE status which is either Signif or notSignif. if you want to do gene IDs you need to switch out. output is two column dataframe where first column is gene symbols second column is signif or not signif
 	library(assertthat)
 	new_df = data.frame(gene_symbol= unique(df$gene_symbol), sig = "notSignif") ##select the columns of interest and only keep unique genes
 	unique_DEGs = unique(df$gene_symbol[which(df$sig == "Signif")])
@@ -23,50 +23,51 @@ format_for_gsea = function(df){
 	new_df
 }
 
+
 ###Load in 
 
 df_all_rosmap = readRDS("rosmap_ad_interaction_results_matrix.RDS") #see AD-DEGs-genome-updates/misc/rosmap_ad_interaction_results_matrix.R
+common = df_all_rosmap[which(!is.na(df_all_rosmap$DE_AD_sign_same)),]
 
-####setup
 
-traits = c("braaksc_simplified", "cogdx_simplified", "ceradsc_simplified")
-pairs = c("hg19_hg18","v30_hg18","v30_hg18", "v43_hg18","T2T_hg18","v30_hg19", "v43_hg19", "T2T_hg19", "v43_v30", "T2T_v30", "T2T_v43")
-
-all = expand.grid(traits, pairs)
-all$filename = paste0(traits, "_",pairs, "msigDB_chromsome", ".txt")
-
-##make the files that are input
-for (i in 1:nrow(all)){
-	df_sig2 = common[which(common$trait == all[i,1]),]
-	df_sig3 = df_sig2[which(df_sig2$assembly_comparison == all[i,2]),]
+##make the files that are input_rosmap
+for (x in unique(common$trait)){
+	print(x)
+	for (i in unique(common$assembly_comparison)){
+	print(i)
+	df_sig2 = common[which(common$trait == x),]
+	df_sig3 = df_sig2[which(df_sig2$assembly_comparison == i),]
 	for_gsea_df = format_for_gsea(data.frame(gene_symbol = df_sig3$gene_symbol, sig = df_sig3$interaction_status))
-	back = as.data.frame(for_gsea_df$gene_ID)
-	write.table(back, paste0("rosmap_common_",all[i,1],"_", all[i,2], "_background_5.13.24.txt"), row.names = T, sep = ",")
-	df = as.data.frame(for_gsea_df$gene_ID[for_gsea_df$DE == "Signif"])
-	write.table(df, paste0("rosmap_common_only_sig","_",all[i,1],"_",all[i,2], "_5.13.24.txt"), row.names = T, sep = ",")
+	for_gsea_df =unique(for_gsea_df)
+	subset_df2 = for_gsea_df$gene_ID
+	subset_df2 = as.data.frame(subset_df2)
+	write.table(subset_df2, paste0("rosmap_common_",x,"_", i, "_background_5.13.24.txt"), row.names = F, sep = ",")
+	sig_subset_df = for_gsea_df[which(for_gsea_df$DE == "Signif"),]
+	sig_subset_df2 =sig_subset_df$gene_ID
+	sig_subset_df2 = as.data.frame(sig_subset_df2)
+	write.table(sig_subset_df2, paste0("rosmap_common_only_sig_",x,"_",i, "_5.13.24.txt"), row.names = F, sep = ",")
+}
 }
 
-##set up
-gene_list_files=list.files("/input",pattern="rosmap_common_only")
-universe_files=list.files("/input",pattern="background")
-all2 = cbind(gene_list_files, universe_files)
-all2 = as.data.frame(all2)
-all2$file_name = gsub("rosmap_common_only_sig_", "",all2$gene_list_files)
-all2$file_name = gsub("_5.13.24.txt", "",all2$file_name)
 
-##run enrichment
-for (i in 1:nrow(all2)){
-	var1=paste0("/input/",all2[i,1])
-	var2=paste0("/input/",all2[i,2])
+##run enrichment for c1
+for (x in unique(common$trait)){
+	for (i in unique(common$assembly_comparison)){
+	var1=paste0("rosmap_common_only_sig_",x,"_",i, "_5.13.24.txt")
+	var2=paste0("rosmap_common_",x,"_", i, "_background_5.13.24.txt")
 	var3="c1.all.v2023.2.Hs.symbols.gmt" #from msigdb website C1 curated gene sets  https://www.gsea-msigdb.org/gsea/msigdb/collections.jsp
-	var4="/output"
-	var5=all2[i,3]
+	var4="/c1/"
+	var5=paste0(x,"_", i, "msigdb")
 	system(paste("python enrich_function.py",var1, var2, var3, var4, var5)) ##see AD-DEGs-genome-updates/misc/enrich_function.py
 }
+}
 
+##pull
 
-path="/output/"
-output_files=list.files("/output/")
+library(data.table)
+
+path="/c1/"
+output_files=list.files("/c1/")
 all=data.table()
     for(x in output_files){
         tmp=fread(paste0(path,x))
@@ -75,4 +76,5 @@ all=data.table()
         }
 
 all = all[order(all$`Adjusted P-value`, decreasing = F),]
-saveRDS(all,"rosmap_final_chromsome.RDS")
+
+saveRDS(all,"rosmap_c1_msigdb.RDS")
